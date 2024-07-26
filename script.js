962b5ff31412abbe827ea48b1cecbe5bf902a8b4
Yussif Muniru <yussifmunirium@mgmail.com>
2024-07-26 17:00:06 +0000

    Stash while trying to get events triggering

--- script.js
+++ script.js
@@ -28,152 +28,307 @@ let abi = [
   },
 ];
 
+contractAddress = "0x67108595Db2367aF7E92F5CE2aEF1a4c2436aFA6";
+abi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "requested",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "available",
+        type: "uint256",
+      },
+    ],
+    name: "InsufficientBalance",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "Send",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "balances",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "receiver",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "mint",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "minter",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "sender",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "receiver",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "send",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
 
- contractAddress = "0x67108595Db2367aF7E92F5CE2aEF1a4c2436aFA6";
- abi = [
-     {
-       inputs: [],
-       stateMutability: "nonpayable",
-       type: "constructor",
-     },
-     {
-       inputs: [
-         {
-           internalType: "uint256",
-           name: "requested",
-           type: "uint256",
-         },
-         {
-           internalType: "uint256",
-           name: "available",
-           type: "uint256",
-         },
-       ],
-       name: "InsufficientBalance",
-       type: "error",
-     },
-     {
-       anonymous: false,
-       inputs: [
-         {
-           indexed: false,
-           internalType: "address",
-           name: "from",
-           type: "address",
-         },
-         {
-           indexed: false,
-           internalType: "address",
-           name: "to",
-           type: "address",
-         },
-         {
-           indexed: false,
-           internalType: "uint256",
-           name: "amount",
-           type: "uint256",
-         },
-       ],
-       name: "Send",
-       type: "event",
-     },
-     {
-       inputs: [
-         {
-           internalType: "address",
-           name: "",
-           type: "address",
-         },
-       ],
-       name: "balances",
-       outputs: [
-         {
-           internalType: "uint256",
-           name: "",
-           type: "uint256",
-         },
-       ],
-       stateMutability: "view",
-       type: "function",
-     },
-     {
-       inputs: [
-         {
-           internalType: "address",
-           name: "receiver",
-           type: "address",
-         },
-         {
-           internalType: "uint256",
-           name: "amount",
-           type: "uint256",
-         },
-       ],
-       name: "mint",
-       outputs: [],
-       stateMutability: "nonpayable",
-       type: "function",
-     },
-     {
-       inputs: [],
-       name: "minter",
-       outputs: [
-         {
-           internalType: "address",
-           name: "",
-           type: "address",
-         },
-       ],
-       stateMutability: "view",
-       type: "function",
-     },
-     {
-       inputs: [
-         {
-           internalType: "address",
-           name: "sender",
-           type: "address",
-         },
-         {
-           internalType: "address",
-           name: "receiver",
-           type: "address",
-         },
-         {
-           internalType: "uint256",
-           name: "amount",
-           type: "uint256",
-         },
-       ],
-       name: "send",
-       outputs: [],
-       stateMutability: "nonpayable",
-       type: "function",
-     },
-   ];
-
-
+contractAddress = "0xA77f4b4A68107C9Bde4dc21688b9cE67C74F5CbF";
+abi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "requested",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "available",
+        type: "uint256",
+      },
+    ],
+    name: "InsufficientBalance",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "Send",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "balances",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "user",
+        type: "address",
+      },
+    ],
+    name: "getBalance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "mint",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "minter",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "sender",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "receiver",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "send",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
-
 let signer;
-
-
 let contract;
 
 provider.send("eth_requestAccounts", []).then(() => {
   provider.listAccounts().then((accounts) => {
     signer = provider.getSigner(accounts[0]);
     contract = new ethers.Contract(contractAddress, abi, signer);
+    const eventName = "Send";
+    const eventFilter = contract.filters[eventName]();
+    console.log(eventFilter);
+    contract.on(eventFilter, (sender, receiver, amount, event) => {
+      console.log(
+        "Sender: ",
+        sender,
+        "Receiver: ",
+        receiver,
+        "Amount: ",
+        amount
+      );
+    });
   });
 });
 
-async function setNote() {
-  const note = document.getElementById("note_text").value;
-  await contract.set(note);
+async function mintCoins() {
+  const amount = document.querySelector("#amount").value;
+  await contract.mint(parseInt(amount));
+  console.log("Credit Address: ", await signer.getAddress());
 }
 
-async function getNote() {
-  const note = await contract.get();
-  document.getElementById("fetched_note").innerText = note;
+async function getBalance() {
+  const addr = document.querySelector("#addr").value;
+  console.log(addr);
+  const balance = await contract.getBalance(addr);
+  document.querySelector("#avail_bal").innerText = balance;
+}
+
+async function sendCoins() {
+  const sendersAddress = await signer.getAddress();
+  const addr = document.querySelector("#addr").value;
+  const amount = document.querySelector("#amount").value;
+  await contract.send(sendersAddress, addr, parseInt(amount));
+  console.log("Sent successfully.", sendersAddress, addr);
 }
